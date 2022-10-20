d00bd7119a666c1fce8c8fd696fad274a46dc6ae
Brycen Baptist <k15brbapt@DESKTOP-C671EHD.localdomain>
2022-10-19 19:16:33 -0500

    Third push

--- script.js
+++ script.js
@@ -1,11 +1,83 @@
-function hello (){
-    var blah = "its early in the morning";
-    console.log(blah);
-}
+//function hello (){
+   // var blah = "its early in the morning";
+    //console.log(blah);
+//}
 
-var blah = "please give me tea";
+//var blah = "please give me tea";
 
-hello();
+//hello();
 
-console.log(blah);
+//console.log(blah);
 
+
+//var names = ["reed", "susan", "ben", "john"];
+
+
+//var pets = ['moxxi', 'pickle', 'hooch','monkeybutt',];
+
+//var newanimal = pets.slice(1,3);
+//var newanimal2 = pets.slice(2,3)
+//var newanimal3 = pets.slice(2,4);
+//alert(newanimal3);
+
+
+//var numbers = [1, 2, 3, 4, 5];
+
+//for (var i = 0; i < numbers.length; i++){
+    //numbers [i] = numbers[i]*2;
+//}
+//alert(numbers);
+
+
+//var numbers = [1, 2, 3, 4, 5];
+
+//numbers.forEach(function(element){
+    //alert(element + 1);
+//})
+
+//var pets = ["moxxi", "pickle", "hooch", "nori"];
+//pets.forEach(function(element){
+//document.write("<h1>" + element + "is awesome" + "<h1>");
+//})
+
+//var words = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+
+//var longwords = words.filter (function(thisword){
+    //return thisword.length > 5;
+//});
+//alert(longwords);
+
+//pets.sort();
+//alert(pets);
+
+//var points = [40, 100, 1, 5, 25, 10];
+//points.sort(function(a, b){return a-b});
+//var things = ['word', 'Word', '1 word', '2 words'];
+//things.sort();
+
+//alert(points);
+//alert(things);
+
+//var txt = '    I like oranges.   '
+//var newtxt = txt.trim();
+
+//newtxt.length;
+
+//var words = 'I love talking to animals.';
+//var result = words.slice(7, 14);
+//alert(result);
+
+//var thenanme = "my name is robert jordan, jr";
+//var result = thename.substring(11, theame.length -1);
+
+//alert(result);
+
+//var newname = "my name is rumpelstiltskin";
+//var newresult = newname.substring(11, newname.length - 1);
+
+//alert(newresult);
+
+var test = "thIs Is A buNcH of RanDOm WoRds";
+var result = test.toUpperCase();
+
+alert(result);
